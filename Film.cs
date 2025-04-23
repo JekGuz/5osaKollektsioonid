63870d5f6d57a6a1542aff8df7d2c8c46238d5f2
«JekGuz» <jekaterina.guzek@gmail.com>
2025-04-23 16:57:22 +0300

    filmid ule 5

---
+++ Film.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5osaKollektsioonid
+{
+    class Film
+    {
+        // нужен для структуры на будущее в фильмах
+        public string Pealkiri { get; set; }
+        public int Aasta { get; set; }
+        public string Zanr { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Pealkiri} ({Aasta}) - Žanr: {Zanr}";
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -11,6 +11,10 @@ namespace _5osaKollektsioonid
     {
         static void Main(string[] args)
         {
+
+
+
+
             // Array - одного типа данные
             Console.WriteLine("----------------------------- ArrayList -------------------------------------------");
             ArrayList nimed = new ArrayList();
@@ -45,6 +49,64 @@ namespace _5osaKollektsioonid
             Console.WriteLine("----------------------------- Dictinary -------------------------------------------");
             Dictionary d = new Dictionary();
             d.dicti();
+
+
+            // Список фильмов - 
+            List<Film> filmid = new List<Film>()
+            {
+                new Film() { Pealkiri = "Pride and Prejudice", Aasta = 2005, Zanr = "Draama" },
+                new Film() { Pealkiri = "The Notebook", Aasta = 2004, Zanr = "Romantika" },
+                new Film() { Pealkiri = "Little Women", Aasta = 2019, Zanr = "Ajalooline" },
+                new Film() { Pealkiri = "The Devil Wears Prada", Aasta = 2006, Zanr = "Komöödia" },
+                new Film() { Pealkiri = "Amélie", Aasta = 2001, Zanr = "Romantika" },
+                new Film() { Pealkiri = "La La Land", Aasta = 2016, Zanr = "Muusikal" },
+                new Film() { Pealkiri = "The Intern", Aasta = 2015, Zanr = "Komöödia" },
+                new Film() { Pealkiri = "Eat Pray Love", Aasta = 2010, Zanr = "Eneseleidmine" },
+                new Film() { Pealkiri = "The Age of Adaline", Aasta = 2015, Zanr = "Romantika" },
+                new Film() { Pealkiri = "Memoirs of a Geisha", Aasta = 2005, Zanr = "Draama" }
+            };
+
+            // 1. Ищим фильм по жанру
+            Console.WriteLine("------------------------Leiavad kõik filmid, mis kuuluvad kindlasse žanrisse------------------------");
+            Console.WriteLine("Sisesta žanr:");
+            string zanr = Console.ReadLine();
+
+            foreach (Film f in filmid)
+            {
+                if (f.Zanr == zanr)
+                    Console.WriteLine($"{f.Pealkiri} ({f.Aasta})");
+            }
+
+            // 2. Ищим самый новый фильм по годам
+            Console.WriteLine("------------------------Leiavad uusima filmi------------------------");
+            Film uusim = filmid[0];
+            foreach (Film f in filmid)
+            {
+                if (f.Aasta > uusim.Aasta)
+                    uusim = f;
+            }
+            Console.WriteLine($"Uusim film: {uusim.Pealkiri} ({uusim.Aasta})");
+
+            // 3. Группируем по жанру
+            Console.WriteLine("------------------------Grupeerivad filmid žanrite kaupa------------------------");
+            Dictionary<string, List<Film>> grupeeritud = new Dictionary<string, List<Film>>();
+
+            foreach (Film f in filmid)
+            {
+                if (grupeeritud.ContainsKey(f.Zanr) == false)
+                {
+                    grupeeritud[f.Zanr] = new List<Film>();
+                }
+
+                grupeeritud[f.Zanr].Add(f);
+            }
+
+            foreach (var paar in grupeeritud)
+            {
+                Console.WriteLine("Žanr: " + paar.Key);
+                foreach (Film f in paar.Value)
+                    Console.WriteLine($" - {f.Pealkiri}({f.Aasta})");
+            }
 
         }
     }
